aefa95309e8e50e811ad0e51372c95037c9aaf52
HamzaMoinet <moinethamza0@gmail.com>
2025-01-13 10:52:16 +0100

    factorial

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,15 +5,8 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1
+        n -= 1  
     return result
 
-# Vérification si un argument a été passé
-if len(sys.argv) < 2:
-    print("Erreur : Veuillez passer un argument pour le calcul du factoriel.")
-else:
-    try:
-        f = factorial(int(sys.argv[1]))
-        print(f)
-    except ValueError:
-        print("Erreur : L'argument doit être un nombre entier.")
+f = factorial(int(sys.argv[1]))
+print(f)
